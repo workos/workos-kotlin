dc0c2208a1da520f95e1a7998feb0aa23cadd4cf
Rob Frampton <rob@workos.com>
2021-10-25 11:12:50 -0400

    Configure publishing to local maven repository

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,3 +1,4 @@
+group = "com.workos"
 version = "0.0.1"
 
 plugins {
@@ -6,9 +7,14 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 
     `java-library`
+
+    `maven-publish`
 }
 
-repositories { mavenCentral() }
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
@@ -44,6 +50,16 @@ tasks.jar {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "workos"
+
+            from(components["java"])
+        }
     }
 }
 
