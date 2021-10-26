cb0886e64b036f779bb0c82dac38da3e59f9ae4d
David Liu <david.liu@workos.com>
2021-10-26 14:48:48 -0700

    remove unused param

--- lib/src/main/kotlin/com/workos/common/http/RequestConfig.kt
+++ lib/src/main/kotlin/com/workos/common/http/RequestConfig.kt
@@ -1,3 +1,3 @@
 package com.workos.common.http
 
-class RequestConfig(val headers: Map<String, String> = mapOf(), val params: Map<String, String> = mapOf(), val data: Any)
+class RequestConfig(val headers: Map<String, String> = mapOf(), val params: Map<String, String> = mapOf())
