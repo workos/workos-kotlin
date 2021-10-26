641c4ae77df3ca583f3bcbc59fedafca18abf8e4
Rob Frampton <rob@workos.com>
2021-10-26 14:33:57 -0400

    Update connection types

--- lib/src/main/kotlin/com/workos/sso/models/Connection.kt
+++ lib/src/main/kotlin/com/workos/sso/models/Connection.kt
@@ -35,15 +35,22 @@ data class Connection
 
 enum class ConnectionType(val type: String) {
     ADFSSAML("ADFSSAML"),
+    Auth0SAML("Auth0SAML"),
     AzureSAML("AzureSAML"),
+    CyberArkSAML("CyberArkSAML"),
     GenericOIDC("GenericOIDC"),
     GenericSAML("GenericSAML"),
     GoogleOAuth("GoogleOAuth"),
     GoogleSAML("GoogleSAML"),
+    JumpCloudSAML("JumpCloudSAML"),
     MagicLink("MagicLink"),
+    MicrosoftOAuth("MicrosoftOAuth"),
     OktaSAML("OktaSAML"),
     OneLoginSAML("OneLoginSAML"),
     PingFederateSAML("PingFederateSAML"),
     PingOneSAML("PingOneSAML"),
+    RipplingSAML("RipplingSAML"),
+    SalesforceSAML("SalesforceSAML"),
+    ShibbolethSAML("ShibbolethSAML"),
     VMwareSAML("VMwareSAML"),
 }
