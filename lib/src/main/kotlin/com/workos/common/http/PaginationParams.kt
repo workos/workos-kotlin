42aff1e8aa0e249e8529903b8225e8f30cb0555d
David Liu <david.liu@workos.com>
2021-10-27 09:38:55 -0700

    Refactor pagination params

--- lib/src/main/kotlin/com/workos/common/http/PaginationParams.kt
+++ lib/src/main/kotlin/com/workos/common/http/PaginationParams.kt
@@ -1,16 +1,10 @@
 package com.workos.common.http
 
-class PaginationParams private constructor(params: MutableMap<String, String>) : HashMap<String, String>() {
-    init {
-        for ((key, value) in params) {
-            this[key] = value
-        }
-    }
-
-    data class Builder(private val params: MutableMap<String, String> = mutableMapOf()) {
+class PaginationParams private constructor() : HashMap<String, String>() {
+    data class Builder(private var params: PaginationParams = PaginationParams()) {
         fun after(after: String) = apply { this.params["after"] = after }
         fun before(before: String) = apply { this.params["before"] = before }
         fun limit(limit: Int) = apply { this.params["limit"] = limit.toString() }
-        fun build() = PaginationParams(params)
+        fun build() = params
     }
 }
