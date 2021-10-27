2d20ac036e28eb84443b35aef8059322fbfe82f1
Rob Frampton <rob@workos.com>
2021-10-27 11:01:30 -0400

    Add getProfile and getProfileAndToken

--- lib/src/main/kotlin/com/workos/common/http/RequestConfig.kt
+++ lib/src/main/kotlin/com/workos/common/http/RequestConfig.kt
@@ -1,3 +1,33 @@
 package com.workos.common.http
 
-class RequestConfig(val headers: Map<String, String> = mapOf(), val params: Map<String, String> = mapOf())
+class RequestConfig(
+    val params: Map<String, String>,
+
+    val headers: Map<String, String>,
+
+    val data: Any?,
+) {
+    companion object {
+        fun create(): RequestConfigBuilder {
+            return RequestConfigBuilder()
+        }
+    }
+
+    class RequestConfigBuilder {
+        var params: Map<String, String> = mapOf()
+
+        var headers: Map<String, String> = mapOf()
+
+        var data: Any? = null
+
+        fun params(value: Map<String, String>) = apply { params = value }
+
+        fun headers(value: Map<String, String>) = apply { headers = value }
+
+        fun data(value: Any) = apply { data = value }
+
+        fun build(): RequestConfig {
+            return RequestConfig(params, headers, data)
+        }
+    }
+}
