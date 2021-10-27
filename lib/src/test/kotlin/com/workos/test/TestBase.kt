58b2214fa7f0aa5d3ecec4289465754248e98b8d
Rob Frampton <rob@workos.com>
2021-10-27 15:13:18 -0400

    Improve test utils

--- lib/src/test/kotlin/com/workos/test/TestBase.kt
+++ lib/src/test/kotlin/com/workos/test/TestBase.kt
@@ -3,15 +3,29 @@ package com.workos.test
 import com.github.tomakehurst.wiremock.client.WireMock.* // ktlint-disable no-wildcard-imports
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 import com.github.tomakehurst.wiremock.junit.WireMockRule
+import com.github.tomakehurst.wiremock.matching.StringValuePattern
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import com.workos.WorkOS
 import org.junit.ClassRule
+import kotlin.collections.MutableList
+import kotlin.test.AfterTest
 
 open class TestBase {
+    val stubs = mutableListOf<StubMapping>()
+
     companion object {
         @ClassRule
         @JvmField
         val wireMockRule = WireMockRule(wireMockConfig().dynamicPort().dynamicHttpsPort())
+    }
+
+    @AfterTest
+    fun afterEach() {
+        for (stub in stubs) {
+            deleteStub(stub)
+        }
+
+        stubs.clear()
     }
 
     fun createWorkOSClient(): WorkOS {
@@ -26,9 +40,10 @@ open class TestBase {
         return wireMockRule.port()
     }
 
-    fun stubResponse(url: String, responseBody: String, responseStatus: Int = 200): StubMapping {
-        return stubFor(
+    fun stubResponse(url: String, responseBody: String, responseStatus: Int = 200, params: Map<String, StringValuePattern> = emptyMap()): StubMapping {
+        val stub = stubFor(
             any(urlPathEqualTo(url))
+                .withQueryParams(params)
                 .willReturn(
                     aResponse()
                         .withStatus(responseStatus)
@@ -36,9 +51,12 @@ open class TestBase {
                         .withHeader("X-Request-ID", "request_id_value")
                 )
         )
+        stubs.add(stub)
+
+        return stub
     }
 
-    fun deleteStub(stub: StubMapping) {
+    private fun deleteStub(stub: StubMapping) {
         removeStub(stub)
     }
 }
