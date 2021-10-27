c2bc5d28bdb0efa337defaa0cc8b186f34d59a35
David Liu <david.liu@workos.com>
2021-10-27 10:49:56 -0700

    Update to new builder Api

--- lib/src/main/kotlin/com/workos/common/http/PaginationParams.kt
+++ lib/src/main/kotlin/com/workos/common/http/PaginationParams.kt
@@ -1,7 +1,32 @@
 package com.workos.common.http
 
-class PaginationParams private constructor() : HashMap<String, String>() {
-    data class Builder(private var params: PaginationParams = PaginationParams()) {
+class PaginationParams @JvmOverloads constructor(
+    after: String? = null,
+    before: String? = null,
+    limit: Int? = null
+) : HashMap<String, String>() {
+
+    init {
+        if (after != null) {
+            set("after", after)
+        }
+
+        if (before != null) {
+            set("before", before)
+        }
+
+        if (limit != null) {
+            set("limit", limit.toString())
+        }
+    }
+
+    companion object {
+        fun builder(): Builder {
+            return Builder()
+        }
+    }
+
+    class Builder(private var params: PaginationParams = PaginationParams()) {
         fun after(after: String) = apply { this.params["after"] = after }
         fun before(before: String) = apply { this.params["before"] = before }
         fun limit(limit: Int) = apply { this.params["limit"] = limit.toString() }
