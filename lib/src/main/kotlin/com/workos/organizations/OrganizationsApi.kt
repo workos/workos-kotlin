21f7f61cf2e5b205bb37d54c916ca34c952a5d01
Rob Frampton <rob@workos.com>
2021-10-28 11:31:23 -0400

    Add deleteOrganization method

--- lib/src/main/kotlin/com/workos/organizations/OrganizationsApi.kt
+++ lib/src/main/kotlin/com/workos/organizations/OrganizationsApi.kt
@@ -4,6 +4,10 @@ import com.workos.WorkOS
 import com.workos.organizations.models.Organization
 
 class OrganizationsApi(val workos: WorkOS) {
+  fun deleteOrganization(id: String) {
+    workos.delete("/organizations/$id")
+  }
+
   fun getOrganization(id: String): Organization {
     return workos.get("/organizations/$id", Organization::class.java)
   }
