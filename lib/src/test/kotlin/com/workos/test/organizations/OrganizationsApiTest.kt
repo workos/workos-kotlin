ea02102f02a3ae6cf6c562971900bc0baa0b3b3a
Robert Frampton <rob@workos.com>
2021-10-28 12:35:27 -0400

    Add deleteOrganization method (#26)

--- lib/src/test/kotlin/com/workos/test/organizations/OrganizationsApiTest.kt
+++ lib/src/test/kotlin/com/workos/test/organizations/OrganizationsApiTest.kt
@@ -1,11 +1,46 @@
 package com.workos.test.organizations
 
 import com.github.tomakehurst.wiremock.client.WireMock.* // ktlint-disable no-wildcard-imports
+import com.workos.common.exceptions.UnauthorizedException
 import com.workos.test.TestBase
+import org.junit.jupiter.api.Assertions.assertThrows
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class OrganizationsApiTest : TestBase() {
+  @Test
+  fun deleteOrganizationShouldNotError() {
+    val workos = createWorkOSClient()
+
+    val organizationId = "org_01FJYCNTB6VC4K5R8BTF86286Q"
+
+    stubResponse(
+      "/organizations/$organizationId",
+      "{}"
+    )
+
+    val response = workos.organizations.deleteOrganization(organizationId)
+
+    assertEquals(Unit, response)
+  }
+
+  @Test
+  fun deleteOrganizationShouldThrowError() {
+    val workos = createWorkOSClient()
+
+    val organizationId = "org_01FJYCNTB6VC4K5R8BTF86286Q"
+
+    stubResponse(
+      "/organizations/$organizationId",
+      "{}",
+      401
+    )
+
+    assertThrows(UnauthorizedException::class.java) {
+      workos.organizations.deleteOrganization(organizationId)
+    }
+  }
+
   @Test
   fun getOrganizationShouldReturnPayload() {
     val workos = createWorkOSClient()
