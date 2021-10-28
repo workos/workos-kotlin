f07966e8b21bcbd60400f3ae7541a1dd2d71bfd6
Rob Frampton <rob@workos.com>
2021-10-28 14:10:13 -0400

    Add createOrganization to organizations module

--- lib/src/main/kotlin/com/workos/organizations/OrganizationsApi.kt
+++ lib/src/main/kotlin/com/workos/organizations/OrganizationsApi.kt
@@ -1,5 +1,6 @@
 package com.workos.organizations
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.workos.WorkOS
 import com.workos.common.http.PaginationParams
 import com.workos.common.http.RequestConfig
@@ -7,6 +8,47 @@ import com.workos.organizations.models.Organization
 import com.workos.organizations.models.OrganizationList
 
 class OrganizationsApi(val workos: WorkOS) {
+  class CreateOrganizationOptions(
+    val name: String? = null,
+
+    @JsonProperty("allow_profiles_outside_organization")
+    val allowProfilesOutsideOrganization: Boolean? = null,
+
+    val domains: List<String>? = null
+  ) {
+    class Builder {
+      var name: String? = null
+
+      var allowProfilesOutsideOrganization: Boolean? = null
+
+      var domains: List<String>? = null
+
+      fun name(value: String) = apply { name = value }
+
+      fun allowProfilesOutsideOrganization(value: Boolean) = apply { allowProfilesOutsideOrganization = value }
+
+      fun domains(value: List<String>) = apply { domains = value }
+
+      fun build(): CreateOrganizationOptions {
+        return CreateOrganizationOptions(name, allowProfilesOutsideOrganization, domains)
+      }
+    }
+
+    companion object {
+      fun builder(): Builder {
+        return Builder()
+      }
+    }
+  }
+
+  fun createOrganization(options: CreateOrganizationOptions = CreateOrganizationOptions()): Organization {
+    val config = RequestConfig.builder()
+      .data(options)
+      .build()
+
+    return workos.post("/organizations", Organization::class.java, config)
+  }
+
   fun deleteOrganization(id: String) {
     workos.delete("/organizations/$id")
   }
