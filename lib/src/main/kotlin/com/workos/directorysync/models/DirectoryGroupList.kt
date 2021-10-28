6b2f9d0846ee1be92457eb17b61c4fc3843f79a3
David Liu <david.liu@workos.com>
2021-10-28 10:02:14 -0700

    fix import

--- lib/src/main/kotlin/com/workos/directorysync/models/DirectoryGroupList.kt
+++ lib/src/main/kotlin/com/workos/directorysync/models/DirectoryGroupList.kt
@@ -1,6 +1,7 @@
 package com.workos.directorysync.models
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.workos.common.models.ListMetadata
 
 data class DirectoryGroupList
