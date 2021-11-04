e6ebc400556f23ed453d29694ef8349a0422c464
David Liu <david.liu@workos.com>
2021-11-04 16:19:15 -0700

    Add JavaDocs for DirectorySync

--- workos/src/main/kotlin/com/workos/directorysync/models/DirectoryList.kt
+++ workos/src/main/kotlin/com/workos/directorysync/models/DirectoryList.kt
@@ -4,6 +4,13 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.workos.common.models.ListMetadata
 
+/**
+ * A list of WorkOS [Directory] resources. This class is not meant to be
+ * instantiated directly.
+ *
+ * @param data A list of [Directory].
+ * @param listMetadata [com.workos.common.models.ListMetadata].
+ */
 data class DirectoryList
 @JsonCreator constructor(
   @JvmField
--- workos/src/main/kotlin/com/workos/directorysync/models/DirectoryState.kt
+++ workos/src/main/kotlin/com/workos/directorysync/models/DirectoryState.kt
@@ -2,8 +2,22 @@ package com.workos.directorysync.models
 
 import com.fasterxml.jackson.annotation.JsonValue
 
+/**
+ * An enumeration of states for a [Directory]
+ *
+ * @param state The Directory State string value.
+ */
 enum class DirectoryState(@JsonValue val state: String) {
+  /**
+   * The directory is unable to sync due to invalid credentials
+   */
   InvalidCredentials("invalid_credentials"),
+  /**
+   * The directory is successfully linked and will sync.
+   */
   Linked("linked"),
+  /**
+   * The directory is not linked and will not sync.
+   */
   Unlinked("unlinked"),
 }
