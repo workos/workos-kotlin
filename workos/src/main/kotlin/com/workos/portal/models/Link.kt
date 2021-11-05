eadc6f3fadb4a4187ed71937c50878df66b598c5
David Liu <david.liu@workos.com>
2021-11-05 16:24:47 -0700

    Add JavaDocs for Admin Portal

--- workos/src/main/kotlin/com/workos/portal/models/Link.kt
+++ workos/src/main/kotlin/com/workos/portal/models/Link.kt
@@ -2,6 +2,11 @@ package com.workos.portal.models
 
 import com.fasterxml.jackson.annotation.JsonCreator
 
+/**
+ * The response object from creating a link.
+ *
+ * @param link The generated URL for access to the Admin Portal.
+ */
 data class Link
 @JsonCreator constructor(
   @JvmField
