18dbfd5a3ca5a9976e6fb4c3ec04361da06ee2e3
David <86431031+dliu-workos@users.noreply.github.com>
2021-11-05 14:24:16 -0700

    Update JavaDocs for SSO (#64)

--- workos/src/main/kotlin/com/workos/sso/models/ConnectionDomain.kt
+++ workos/src/main/kotlin/com/workos/sso/models/ConnectionDomain.kt
@@ -2,6 +2,13 @@ package com.workos.sso.models
 
 import com.fasterxml.jackson.annotation.JsonCreator
 
+/**
+ * Describes the domain records associated with a [Connection]. This
+ * class is not meant to be instantiated directly.
+ *
+ * @param domain The Domain for a connection record.
+ * @param id Unique identifier for a Connection Domain.
+ */
 data class ConnectionDomain
 @JsonCreator constructor(
   @JvmField
