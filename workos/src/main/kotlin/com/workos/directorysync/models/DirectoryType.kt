0e5471176d181b28314cae600bd677017a02275f
David <86431031+dliu-workos@users.noreply.github.com>
2021-11-05 08:57:06 -0700

    Add JavaDocs for DirectorySync (#61)

--- workos/src/main/kotlin/com/workos/directorysync/models/DirectoryType.kt
+++ workos/src/main/kotlin/com/workos/directorysync/models/DirectoryType.kt
@@ -2,18 +2,62 @@ package com.workos.directorysync.models
 
 import com.fasterxml.jackson.annotation.JsonValue
 
+/**
+ * An enumeration of types for a [Directory].
+ *
+ * @param state The Directory Type string value.
+ */
 enum class DirectoryType(@JsonValue val type: String) {
+  /**
+   * Azure SCIM 2.0
+   */
   AzureSCIMV2_0("azure scim v2.0"),
+  /**
+   * BambooHR
+   */
   BambooHr("bamboohr"),
+  /**
+   * FourthHR
+   */
   FourthHr("fourth hr"),
+  /**
+   * Generic SCIM 1.1
+   */
   GenericSCIMV1_1("generic scim v1.1"),
+  /**
+   * Generic SCIM 2.0
+   */
   GenericSCIMV2_0("generic scim v2.0"),
+  /**
+   * Google Workspace https://workspace.google.com/
+   */
   GSuiteDirectory("gsuite directory"),
+  /**
+   * Gusto https://gusto.com/
+   */
   Gusto("gusto"),
+  /**
+   * Hibob https://www.hibob.com/
+   */
   Hibob("hibob"),
+  /**
+   * JumpCloud SCIM https://jumpcloud.com/
+   */
   JumpCloudSCIM2_0("jump cloud scim v2.0"),
+  /**
+   * Okta SCIM 1.1 https://developer.okta.com/docs/reference/scim/scim-11/
+   */
   OktaSCIMV1_1("okta scim v1.1"),
+  /**
+   * Okta SCIM 2.0 https://developer.okta.com/docs/reference/scim/scim-20/
+   */
   OktaSCIMV2_0("okta scim v2.0"),
+  /**
+   * Rippling https://www.rippling.com/
+   */
   Rippling("rippling"),
+  /**
+   * Workday https://www.workday.com/
+   */
   Workday("workday"),
 }
--- workos/src/main/kotlin/com/workos/directorysync/models/UserState.kt
+++ workos/src/main/kotlin/com/workos/directorysync/models/UserState.kt
@@ -2,7 +2,18 @@ package com.workos.directorysync.models
 
 import com.fasterxml.jackson.annotation.JsonValue
 
+/**
+ * An enumeration of states for a Directory [User].
+ *
+ * @param state The User State string value.
+ */
 enum class UserState(@JsonValue val state: String) {
+  /**
+   * The user is active.
+   */
   Active("active"),
+  /**
+   * The user is suspended.
+   */
   Suspended("suspended")
 }
