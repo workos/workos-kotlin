97c79711e1bab19a94057a1091d4396cbd2a1e08
David <86431031+dliu-workos@users.noreply.github.com>
2021-11-05 14:12:34 -0700

    Update ProfileAndToken.kt

--- workos/src/main/kotlin/com/workos/sso/models/ProfileAndToken.kt
+++ workos/src/main/kotlin/com/workos/sso/models/ProfileAndToken.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * Represents a [Profile] and an access token that can be used to
- * manage sections. This class is not meant to be instantiated directly.
+ * manage sessions. This class is not meant to be instantiated directly.
  *
  * @param profile [Profile]
  * @param token An access token that can be used to manage sessions like one would a normal OAuth access token. Access tokens are one-time use and expire 10 minutes after they’re created. Session duration is up to the Developer.
