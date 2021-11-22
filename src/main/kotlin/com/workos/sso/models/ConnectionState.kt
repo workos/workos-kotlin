ae8b35a796466c2cf61df33618edbbd713d2da6e
David <86431031+dliu-workos@users.noreply.github.com>
2021-11-22 15:40:50 -0800

    Add `ConnectionState` enum (#84)

---
+++ src/main/kotlin/com/workos/sso/models/ConnectionState.kt
@@ -0,0 +1,23 @@
+package com.workos.sso.models
+
+import com.fasterxml.jackson.annotation.JsonValue
+
+/**
+ * An enumeration of states for a [Connection]
+ *
+ * @param state The Connection State string value.
+ */
+enum class ConnectionState(@JsonValue val state: String) {
+  /**
+   * The connection is active and able to authenticate users.
+   */
+  Active("active"),
+  /**
+   * The connection has been created and requires configuration to be activated.
+   */
+  Draft("draft"),
+  /**
+   * The connection is inactive and unable to authenticate users.
+   */
+  Inactive("inactive"),
+}
