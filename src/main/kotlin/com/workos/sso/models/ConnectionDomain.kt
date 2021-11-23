4534473044bd16fbf9be759a013e257378145a35
David Liu <david.liu@workos.com>
2021-11-22 16:07:29 -0800

    fix connection domain

--- src/main/kotlin/com/workos/sso/models/ConnectionDomain.kt
+++ src/main/kotlin/com/workos/sso/models/ConnectionDomain.kt
@@ -1,16 +1,22 @@
 package com.workos.sso.models
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * Describes the domain records associated with a [Connection]. This
  * class is not meant to be instantiated directly.
  *
+ * @param obj The unique object identifier type of the record.
  * @param domain The Domain for a connection record.
  * @param id Unique identifier for a Connection Domain.
  */
 data class ConnectionDomain
 @JsonCreator constructor(
+  @JvmField
+  @JsonProperty("object")
+  val obj: String = "connection_domain",
+
   @JvmField
   val domain: String,
 
