4512c5dbc6a48beccf96db5bf8519c2478bfef8d
Rob Frampton <rob@workos.com>
2022-02-17 17:09:19 -0500

    Version 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "1.2.0"
+version = "1.2.1"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
