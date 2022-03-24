d86bc1a5a87334f90c7a3ed1d1065501692f6e7e
Patrick_Liao <patrick@workos.com>
2022-03-24 12:07:43 -0500

    Fixes to methods

--- src/main/kotlin/com/workos/mfa/MfaApi.kt
+++ src/main/kotlin/com/workos/mfa/MfaApi.kt
@@ -5,8 +5,10 @@ import com.workos.WorkOS
 import com.workos.common.http.RequestConfig
 import com.workos.mfa.models.Challenge
 import com.workos.mfa.models.Factor
+import com.workos.mfa.models.MfaType
 import com.workos.mfa.models.Sms
 import com.workos.mfa.models.Totp
+import com.workos.mfa.models.VerifyFactorResponse
 import org.apache.http.client.utils.URIBuilder
 
 class MfaApi(private val workos: WorkOS) {
@@ -15,20 +17,20 @@ class MfaApi(private val workos: WorkOS) {
    * Parameters for the [enrollFactor] method.
    */
   class EnrollFactorOptions @JvmOverloads constructor(
-    type: MfaType,
+    type: String,
     issuer: String? = null,
     user: String? = null,
     phoneNumber: String? = null
   ) {
     init {
-      if (type.toString == "generic_otp") set("type", type.toString())
-      if (type.toString == "totp"){
-        set("type", type.toString())
+      if (type == "generic_otp") set("type", type)
+      if (type == "totp"){
+        set("type", type)
         set("issuer", issuer)
         set("user", user)
         }
-      if (type.toString == "sms"){
-        set("type", type.toString())
+      if (type == "sms"){
+        set("type", type)
         set("phoneNumber", phoneNumber)
         }
     }
@@ -67,13 +69,13 @@ class MfaApi(private val workos: WorkOS) {
       .data(challengeFactorOptions)
       .build()
 
-    return workos.post("/auth/factors/challenge", Factor::class.java, config)
+    return workos.post("/auth/factors/challenge", Challenge::class.java, config)
   }
 
   /**
    * Parameters for the [verifyFactor] method.
    */
-  class ChallengeFactorOptions @JvmOverloads constructor(
+  class VerifyFactorOptions @JvmOverloads constructor(
     challengeId: String,
     code: String
   )
