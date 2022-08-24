cbb0fe3c174b1b7ec74da82f3650b090956bf1de
Ash <44040482+AshGodfrey@users.noreply.github.com>
2022-08-24 14:42:41 -0600

    update version to latest (#133)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "2.3.1"
+version = "2.3.2"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
