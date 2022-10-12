2311524ef4aa3e711a0eb740da9302e2215b5c03
Blair Lunceford <74257063+blairlunceford@users.noreply.github.com>
2022-10-12 11:58:38 -0600

    Bump minor version to 2.5.0 (#141)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "2.4.0"
+version = "2.5.0"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
