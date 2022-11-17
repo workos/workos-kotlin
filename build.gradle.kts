e3ac8480c5fd09ee989d0b95d5bd1e5d434e4ae9
adamzinder <96083517+adamzinder@users.noreply.github.com>
2022-11-17 11:47:50 -0800

    Bump version to  2.6.2
    Bumps version to  2.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "2.6.1"
+version = "2.6.2"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
