4fe388c37dad83474af19b6761712a2990df2381
Blair Lunceford <74257063+blairlunceford@users.noreply.github.com>
2024-05-03 14:08:44 -0600

    v3.1.0-beta.user-management3 (#222)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "3.1.0-beta.user-management2"
+version = "3.1.0-beta.user-management3"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
