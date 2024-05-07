a87ebf3c7f86843cbcd1fc18eb76ed5a32f71323
Michael Hadley <m@mthadley.com>
2024-05-07 09:41:19 -0700

    v3.1.0 (#224)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "3.0.1"
+version = "3.1.0"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
