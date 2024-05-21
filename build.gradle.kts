529cb8bf48fa8cd43c0dae3864b9e6f408765cea
Matt Dzwonczyk <9063128+mattgd@users.noreply.github.com>
2024-05-21 14:16:50 -0400

    Bump to version 3.1.0-beta.user-management4. (#227)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "3.1.0-beta.user-management3"
+version = "3.1.0-beta.user-management4"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
