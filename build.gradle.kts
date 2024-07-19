41badb161c35fc9791dbf8fa9136c563e0620e27
Paul Asjes <p.asjes@gmail.com>
2024-07-19 16:53:47 +0200

    clutching at straws here

--- build.gradle.kts
+++ build.gradle.kts
@@ -167,8 +167,8 @@ nexusPublishing {
     create("myNexus") {
       nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
       snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-      username.set(sonatypeUsername)
-      password.set(sonatypePassword)
+      username = sonatypeUsername
+      password = sonatypePassword
     }
   }
 }
