9185971a29539ea1a0c3bfb948f11aec9364e964
Paul Asjes <p.asjes@gmail.com>
2024-07-19 17:09:51 +0200

    Remove myNexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -161,7 +161,7 @@ publishing {
 
 nexusPublishing {
   repositories {
-    create("myNexus") {
+    sonatype {
       nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
       snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
     }
