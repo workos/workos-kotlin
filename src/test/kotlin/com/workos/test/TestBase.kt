38725b47fe345847a94a9efe380b1750a4bc482a
Stanley Phu <stanley@workos.com>
2024-08-13 12:49:15 -0700

    Add stub response method with scenario support

--- src/test/kotlin/com/workos/test/TestBase.kt
+++ src/test/kotlin/com/workos/test/TestBase.kt
@@ -4,6 +4,7 @@ import com.github.tomakehurst.wiremock.client.WireMock.* // ktlint-disable no-wi
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 import com.github.tomakehurst.wiremock.junit.WireMockRule
 import com.github.tomakehurst.wiremock.matching.StringValuePattern
+import com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import com.workos.WorkOS
 import org.junit.ClassRule
@@ -55,6 +56,46 @@ open class TestBase {
           .withBody(responseBody)
           .withHeader("X-Request-ID", "request_id_value")
       )
+
+    if (requestBody != null) {
+      mapping.withRequestBody(equalToJson(requestBody))
+    }
+
+    if (requestHeaders != null) {
+      for (header in requestHeaders.entries.iterator()) {
+        mapping.withHeader(header.key, equalTo(header.value))
+      }
+    }
+
+    val stub = stubFor(mapping)
+
+    stubs.add(stub)
+
+    return stub
+  }
+
+  fun stubResponseWithScenario(
+    url: String,
+    responseBody: String,
+    responseStatus: Int = 200,
+    params: Map<String, StringValuePattern> = emptyMap(),
+    requestBody: String? = null,
+    requestHeaders: Map<String, String>? = null,
+    scenarioName: String,
+    scenarioState: String,
+    nextScenarioState: String
+  ): StubMapping {
+    val mapping = any(urlPathEqualTo(url))
+      .inScenario(scenarioName)
+      .whenScenarioStateIs(scenarioState)
+      .withQueryParams(params)
+      .willReturn(
+        aResponse()
+          .withStatus(responseStatus)
+          .withBody(responseBody)
+          .withHeader("X-Request-ID", "request_id_value")
+      )
+      .willSetStateTo(nextScenarioState)
 
     if (requestBody != null) {
       mapping.withRequestBody(equalToJson(requestBody))
