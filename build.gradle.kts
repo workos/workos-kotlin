318b3308ca5aeeae2cf9763c7c63d929b81754ef
Stanley Phu <stanley@workos.com>
2024-08-13 17:09:27 -0700

    v4.2.0 (#245)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "4.1.0"
+version = "4.2.0"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
