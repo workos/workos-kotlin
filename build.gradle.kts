cd7daa0ec4b2e0e05630e1050152d5e63bf2f5e1
Michael Hadley <m@mthadley.com>
2025-01-15 15:09:56 -0800

    v4.8.0 (#265)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "4.7.0"
+version = "4.8.0"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
