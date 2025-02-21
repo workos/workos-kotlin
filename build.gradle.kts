a10e9e3e8d842b834ac23fd1a7ed0faac07abe7c
Stacy Curry <stacy@workos.com>
2025-02-20 20:42:59 -0600

    v4.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "4.8.0"
+version = "4.9.0"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
