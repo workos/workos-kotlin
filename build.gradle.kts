d05cc820ae803dbb335cdb27439866c7c7a70071
Colin Barber <csrbarber@gmail.com>
2025-10-10 17:23:31 -0300

    v4.16.0 (#297)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.FileOutputStream
 import java.util.Properties
 
 group = "com.workos"
-version = "4.15.0"
+version = "4.16.0"
 
 if (!project.hasProperty("release")) {
   version = "$version-SNAPSHOT"
