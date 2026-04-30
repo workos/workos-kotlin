de6e1c4f19410afa207aefeec29333db746a3b04
Aaron Tainter <amtainter@gmail.com>
2026-04-30 06:13:35 -0700

    feat: Add `user` to organization membership response (#369)

--- src/main/kotlin/com/workos/authorization/models/SlimOrganizationMembership.kt
+++ src/main/kotlin/com/workos/authorization/models/SlimOrganizationMembership.kt
@@ -2,6 +2,7 @@ package com.workos.authorization.models
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.workos.usermanagement.models.User
 import com.workos.usermanagement.types.OrganizationMembershipStatusEnumType
 
 /**
@@ -10,6 +11,11 @@ import com.workos.usermanagement.types.OrganizationMembershipStatusEnumType
  * Unlike the full [com.workos.usermanagement.models.OrganizationMembership],
  * this model omits role, custom_attributes, and other fields not returned
  * by the authorization endpoints.
+ *
+ * The [user] field is only populated by endpoints that explicitly include
+ * the full user object in their response (currently
+ * `GET /authorization/resources/:resource_id/organization_memberships` and
+ * `GET /authorization/organizations/:organization_id/resources/:resource_type_slug/:external_id/organization_memberships`).
  */
 data class SlimOrganizationMembership
 @JsonCreator(mode = JsonCreator.Mode.PROPERTIES) constructor(
@@ -43,5 +49,9 @@ data class SlimOrganizationMembership
 
   @JvmField
   @JsonProperty("updated_at")
-  val updatedAt: String
+  val updatedAt: String,
+
+  @JvmField
+  @JsonProperty("user")
+  val user: User? = null
 )
